79600cec0d882e474a600d30bf34b17a4677a5a4
Valtteri Palonkorpi <ext-valtteri.palonkorpi@elisa.fi>
2024-06-14 14:42:07 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Valtteri Palonkorpi",
   description:
-    "Software Engineer and a Poodle dad with a love for creating things",
+    "Fullstack Developer and a Poodle dad with a love for creating things",
 };
 
 export default function RootLayout({
